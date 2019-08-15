70da3ca15a439e1da5e2bb81e6c55e02ca5b05d7
Cel A. Skeggs <cela@mit.edu>
2019-08-14 21:27:27 -0700

    Fix GC race condition

--- platform/datum/realm.go
+++ platform/datum/realm.go
@@ -1,16 +1,41 @@
 package datum
 
+import "sync"
+
 var TRACE = false
 
 type Realm struct {
-	datums   map[*Datum]struct{}
-	worldRef interface{}
+	busylock         sync.Mutex
+	busy             bool
+	datums           map[*Datum]struct{}
+	deferredRemovals []*Datum
+	worldRef         interface{}
+}
+
+func (r *Realm) setBusy(busy bool) {
+	r.busylock.Lock()
+	defer r.busylock.Unlock()
+
+	if r.busy && busy {
+		panic("already busy!")
+	}
+	if !r.busy && !busy {
+		panic("already not busy!")
+	}
+	r.busy = busy
+	if !busy && r.deferredRemovals != nil {
+		for _, dr := range r.deferredRemovals {
+			delete(r.datums, dr)
+		}
+		r.deferredRemovals = nil
+	}
 }
 
 func (r *Realm) add(d *Datum) {
 	if _, found := r.datums[d]; found {
 		panic("datum already found in realm")
 	}
+	// no busy check here; it's only really for garbage collection, which means remove
 	r.datums[d] = struct{}{}
 	if TRACE {
 		println("added datum", d, "of type", d.Type, "to realm")
@@ -18,16 +43,24 @@ func (r *Realm) add(d *Datum) {
 }
 
 func (r *Realm) remove(d *Datum) {
+	r.busylock.Lock()
+	defer r.busylock.Unlock()
 	if _, found := r.datums[d]; !found {
 		panic("datum not found in realm")
 	}
-	delete(r.datums, d)
+	if r.busy {
+		r.deferredRemovals = append(r.deferredRemovals)
+	} else {
+		delete(r.datums, d)
+	}
 	if TRACE {
 		println("removed datum", d, "of type", d.Type, "from realm")
 	}
 }
 
 func (r *Realm) FindAll(predicate func(IDatum) bool) (out []IDatum) {
+	r.setBusy(true)
+	defer r.setBusy(false)
 	for datum := range r.datums {
 		if predicate(datum.impl) {
 			out = append(out, datum.impl)
@@ -38,6 +71,8 @@ func (r *Realm) FindAll(predicate func(IDatum) bool) (out []IDatum) {
 
 // returns nil if not found
 func (r *Realm) FindOne(predicate func(IDatum) bool) IDatum {
+	r.setBusy(true)
+	defer r.setBusy(false)
 	for datum := range r.datums {
 		if predicate(datum.impl) {
 			return datum.impl
